7cf01c929b82cbe553e733ae7597c19383af977b
biker6286 <ilya.anufriev2014@yandex.ru>
2026-04-09 14:10:53 +0300

    Initial Commit

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Илья Ануфриев on 09.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
